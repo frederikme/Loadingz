6659e457b9a0bcd829be65048399fc90f446e518
Frederik Mees <Fredjemees@gmail.com>
2020-10-04 21:56:56 +0200

    small update

--- loadingbar.py
+++ loadingbar.py
@@ -2,16 +2,16 @@ import sys
 import time
 
 
-class LoadingBar:
+class Loadingbar:
 
     def __init__(self, length_of_loop, explanation="", amount_of_bars=30):
 
         self.length_of_loop = length_of_loop
         self.amount_of_bars = amount_of_bars
         sys.stdout.write("{}\n".format(explanation))
-        self.updateLoadingBar(index=-1)
+        self.update(index=-1)
 
-    def updateLoadingBar(self, index):
+    def update(self, index):
         sys.stdout.write('\r')
 
         percentage_loaded = int((index + 1)*100 / self.length_of_loop)
