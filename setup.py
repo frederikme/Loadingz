b18ff29127b24a5ac2bc6d8034d87060cfb0a331
Frederik Mees <Fredjemees@gmail.com>
2020-10-05 10:22:37 +0200

    making pip package unique

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='loadingz',         # How you named your loadingz folder (MyLib)
   packages=['loadingz'],   # Chose the same as "name"
-  version='0.1',      # Start with a small number and increase it with every change you make
+  version='0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='Tool for displaying loading processes',   # Give a short description about your library
   author='Frederik Mees',                   # Type in your name
